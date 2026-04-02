3824416d07f25a50643e1abaa9842ee75c3a8c24
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2026-04-02 20:40:07 +0600

    Dynamic Segment done

--- src/app/tutorials/[[...slug]]/page.jsx
+++ src/app/tutorials/[[...slug]]/page.jsx
@@ -2,7 +2,8 @@ import Title from "@/Component/Title";
 import React from "react";
 
 const TutorialPages = async ({ params }) => {
-  const { technology, topic, page_no, subject } = await params;
+  const { slug } = await params;
+  const [technology, topic, , page_no, subject] = slug || [];
   return (
     <div>
       <h3 className="p-3 bg-sky-50 mb-5">
@@ -11,12 +12,12 @@ const TutorialPages = async ({ params }) => {
       </h3>
       <Title>{technology} tutorials</Title>
       <div className="p-5">
+        <p>Subject: {subject}</p>
         <div className="flex justify-between">
-          <h2>{topic}</h2>
-          <p>page {page_no}</p>
+          <h2>Topic: {topic}</h2>
+          <p>page: {page_no}</p>
         </div>
         <hr />
-        <p>{subject}</p>
       </div>
     </div>
   );
